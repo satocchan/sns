6a33a74197f3eaa0b02e11a6f68128ddff8cdbe4
satoshi <sts3104sbh@gmail.com>
2022-03-20 10:24:42 +0000

    sns conplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+  
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
+   
+  get 'users/index' => 'users#index'
+  get 'signup' => 'users#new'
+  post 'users/create' => 'users#create'
+  get 'users/:id' => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
+  get "users/:id/likes" => "users#likes"
+  
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+  
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
 end
---
+++ db/migrate/20220315134737_add_password_column.rb
@@ -0,0 +1,5 @@
+class AddPasswordColumn < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password, :string
+  end
+end
